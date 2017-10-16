6bb234b5c0d35f39be69b55b15a8936fc6e6c933
jirayuch01 <jirayu_ch01@hotmail.com>
2017-10-16 21:35:33 +0700

    Design UI
    Design UI

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+$(window).on('load', function() {
+    $('.heart pulse2').fadeOut();
+    $('#preloader').delay(500).fadeOut('slow');
+    $('body').delay(350).css({
+        'overflow': 'visible'
+    });
+})
